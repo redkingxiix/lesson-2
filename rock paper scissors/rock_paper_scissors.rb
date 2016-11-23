a16aa841b2f0a6e2ca5b30cf8166c8df4a634f8d
Rory Brookes <rorybrookes89@hotmail.com>
2016-11-23 15:15:42 +0900

    rearrange files

--- rock paper scissors/rock_paper_scissors.rb
+++ rock paper scissors/rock_paper_scissors.rb
@@ -1,23 +1,26 @@
 
-VALID_CHOICES = ['rock','paper','scissors']
+VALID_CHOICES = %w(rock paper scissors)
 
 def prompt(message)
   Kernel.puts("=>#{ message}")
 end
 
+def win?(player1, player2)
+  (player1 == 'rock' && player2 == 'scissors') ||
+    (player1 == 'paper' && player2 == 'rock') ||
+    (player1 == 'scissors' && player2 == 'paper')
+end
+
 def display_results(player, computer)
- if(player == 'rock' && computer == 'scissors') ||
-    (player == 'paper' && computer == 'rock') ||
-    (player == 'scissors' && computer == 'paper')
-    prompt("You won!") 
-  elsif(computer == 'rock' && player == 'scissors') ||
-    (computer == 'paper' && player == 'rock') ||
-    (computer == 'scissors' && player == 'paper')
+  if win?(player, computer)
+    prompt("You win!")
+  elsif win?(computer, player)
     prompt("Computer won!") 
   else
     prompt("Tie game!")
   end
 end
+
 loop do
   choice = ''
   loop do
---
+++ rock paper scissors/test_method.rb
@@ -0,0 +1,12 @@
+
+def test_method
+  prompt('test message')
+end
+
+test_method           # second, try invoking "test_method" here
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+test_method           # first, try invoking "test_method" here
